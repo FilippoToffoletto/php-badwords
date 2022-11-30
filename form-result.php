4bd0fe0b8060d61eb095b661eb9744c3282f9c56
FilippoToffoletto <filippotoffoletto@hotmail.it>
2022-11-30 16:22:29 +0100

    [add]censored word and lenght

--- form-result.php
+++ form-result.php
@@ -1,6 +1,10 @@
 <?php
 $name = $_POST['nome'];
 $lastname = $_POST['cognome'];
+// $lastname_explode= explode('',$lastname);
+
+// $censored = str_replace($lastname, '***', $lastname);
+
 ?>
 
 <!DOCTYPE html>
@@ -10,14 +14,23 @@ $lastname = $_POST['cognome'];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
     <title>Form Result</title>
 </head>
 <body>
 
-    <div class="container">
-        <h1 class="text-capitalize py-5">
-            ciao <?php echo $name ?> <?php echo $lastname ?>
-        </h1>
+    <div class="main">
+        <div class="container">
+            <h1 class="text-capitalize py-5 text-center">
+                Ciao <?php echo $name ?> <?php echo str_replace($lastname, '***', $lastname) ?>
+            </h1>
+            <h2 class="text-capitalize">
+                il tuo cognome è lungo <span class="fs-1"><?php echo strlen($lastname) ?></span> lettere
+            </h2>
+            <p class="w-50 my-3 py-2 px-4">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt laboriosam ipsam perspiciatis doloremque sequi consequuntur odio dolore dolor laudantium accusamus Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad rerum, pariatur ut natus delectus eos consequuntur odio dolor. Iure laboriosam <?php echo $lastname?> consectetur quidem maiores animi impedit? Facilis tempore reprehenderit accusamus veritatis tempora, soluta enim voluptate numquam voluptatum, earum odio atque asperiores aut obcaecati sapiente eum labore dicta, quod minus sint accusantium.
+            </p>
+        </div>
     </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -5,29 +5,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
     <title>Php-badwords</title>
 </head>
 <body>
-    <?php 
-    // DICHIARO LA VARIABILE
-    // $nome = $_GET['name'] . ' ' . $_GET['lastname'];
-    ?>
-
-    <div class="container py-5 m-auto">
-    <form action="./form-result.php" method="POST">
-        <div class="row">
-            <div class="col">
-                <input type="text" class="form-control" placeholder="First name" name="nome">
-            </div>
-            <div class="col">
-                <input type="text" class="form-control" placeholder="Last name" aria-label="Last name" name="cognome">
-            </div>
-            <div class="col">
-                <button type='submit' class="btn btn-outline-primary" reset="input">INVIA</button>
-            </div>
+    <div class="main">
+        <div class="container py-5 m-auto">
+            <h1 class="text-capitalize">inserisci il tuo nome e cognome per continuare</h1>
+            <form action="./form-result.php" method="POST">
+                <div class="row row-cols-1 py-3">
+                    <div class="col-lg-3 py-2">
+                        <input type="text" class="form-control" placeholder="First name" name="nome">
+                    </div>
+                    <div class="col-lg-4 py-2">
+                        <input type="text" class="form-control" placeholder="Last name" aria-label="Last name" name="cognome">
+                    </div>
+                    <div class="col py-2">
+                        <button type='submit' class="btn btn-outline-primary" reset="input">INVIA</button>
+                    </div>
+                </div>
+            </form>
         </div>
-    </form>
     </div>
+    
             
 </body>
 </html>
